21aa28afe72b669f54f3e386ebaf39a2acfaf6bb
Yifan <yz542@cornell.edu>
2015-10-10 11:48:12 -0400

    [frontend]setting up the structure

---
+++ Round_for_Change/tableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  tableViewCell.m
+//  Round_for_Change
+//
+//  Created by Yifan Zhu on 10/10/15.
+//  Copyright (c) 2015 FinTech. All rights reserved.
+//
+
+#import "tableViewCell.h"
+
+@implementation tableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
