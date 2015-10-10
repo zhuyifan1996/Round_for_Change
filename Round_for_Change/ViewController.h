13811b09a5ee95e8c20d713c0ed66af4f5e86458
Yifan <yz542@cornell.edu>
2015-10-10 10:48:50 -0400

    initial commit

---
+++ Round_for_Change/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Round_for_Change
+//
+//  Created by Yifan Zhu on 10/10/15.
+//  Copyright (c) 2015 FinTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Round_for_Change/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Round_for_Change
+//
+//  Created by Yifan Zhu on 10/10/15.
+//  Copyright (c) 2015 FinTech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
